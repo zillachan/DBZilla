5ec3fac79f11200b2eaf8b4599fed93c24092526
Zilla Chen <zillachan@gmail.com>
2017-01-24 11:26:07 +0800

    <imp zilla>update copyright

--- app/src/androidTest/java/com/zilla/dbzilla/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/zilla/dbzilla/ExampleInstrumentedTest.java
@@ -1,19 +1,25 @@
+/*
+ * Copyright 2017. Zilla Chen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.zilla.dbzilla;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import com.github.snowdream.android.util.Log;
-import com.zilla.dbzilla.db.DBHelper;
-import com.zilla.dbzilla.db.ZillaDB;
 import org.junit.*;
 import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
