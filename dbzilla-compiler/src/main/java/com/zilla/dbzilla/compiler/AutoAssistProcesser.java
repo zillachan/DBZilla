d7a12e5d4a7f339c1fcb94924e9f81256c4294ee
Zilla Chen <zillachan@gmail.com>
2017-02-04 17:40:37 +0800

    <imp zilla>add compile annotation support

---
+++ dbzilla-compiler/src/main/java/com/zilla/dbzilla/compiler/AutoAssistProcesser.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017. Zilla Chen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zilla.dbzilla.compiler;
+
+import android.database.sqlite.SQLiteStatement;
+import com.google.auto.service.AutoService;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import com.zilla.dbzilla.annotations.AutoAssist;
+
+import javax.annotation.processing.*;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+@AutoService(Processor.class)
+public final class AutoAssistProcesser extends AbstractProcessor {
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(AutoAssist.class.getCanonicalName());
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Set<? extends Element> set = roundEnv.getElementsAnnotatedWith(AutoAssist.class);
+        for (Element element : set) {
+            if (element.getKind() != ElementKind.CLASS) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "only support class");
+            }
+            /*生成方法*/
+            MethodSpec creaedMethod = MethodSpec.methodBuilder("bind")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .returns(void.class)
+                    .addParameter(SQLiteStatement.class, "stat")
+                    .addStatement("System.out.println($S)", "this`s java source is created by dynamic")
+                    .build();
+            TypeSpec createdClass = TypeSpec.classBuilder(element.getSimpleName().toString()).addModifiers(Modifier.PUBLIC).addMethod(creaedMethod).build();//指定生成的类
+            JavaFile javaFile = JavaFile.builder("com.zilla.xxxx", createdClass).build();
+
+            try {
+                javaFile.writeTo(processingEnv.getFiler());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }
+}
